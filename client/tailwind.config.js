b402713f8ba1e2029b609fd4d47531dd0540aade
Saad Khaleeq <saadkhaleeq610@gmail.com>
2025-02-08 14:56:55 +0500

    Frontend Shadcn Setup successful

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-    theme: {
-    	extend: {
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		colors: {}
-    	}
-    },
-    plugins: [require("tailwindcss-animate")],
-  }
-  
+  theme: {
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
+  },
+  // plugins: [require("tailwindcss-animate")],
+}
+
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
