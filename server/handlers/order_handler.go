ca3e1fee822afead74fb746867878d8d61ebcfff
Samama251251 <samama251251@gmail.com>
2025-02-22 21:52:47 +0500

    Added the order handler

--- server/handlers/order_handler.go
+++ server/handlers/order_handler.go
@@ -23,10 +23,13 @@ type listOrdersRequest struct {
 	CustomerID int32 `json:"customer_id" binding:"required"`
 }
 
-type updateOrderStatusRequest struct {
-	OrderID int32  `json:"order_id" binding:"required"`
-	Status  string `json:"status" binding:"required"`
-}
+// type createMenuItemRequest struct {
+// 	RestaurantID int32  `json:"restaurant_id" binding:"required"`
+// 	Name         string `json:"name" binding:"required"`
+// 	Description  string `json:"description" binding:"required"`
+// 	Price        string `json:"price" binding:"required"`
+// 	IsAvailable  *bool  `json:"is_available"` // Optional; defaults to true if omitted
+// }
 
 func CreateOrder(store *db.Queries) gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -84,21 +87,42 @@ func ListOrders(store *db.Queries) gin.HandlerFunc {
 	}
 }
 
-func UpdateOrderStatus(store *db.Queries) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var req updateOrderStatusRequest
-		if err := c.ShouldBindJSON(&req); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		err := store.UpdateOrderStatus(c, db.UpdateOrderStatusParams{
-			Status:  req.Status,
-			OrderID: req.OrderID,
-		})
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update order status"})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{"message": "Order status updated successfully"})
-	}
-}
+// It is not working as of now need to fix it
+// func CreateMenuItem(store *db.Queries) gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		var req createMenuItemRequest
+// 		if err := c.ShouldBindJSON(&req); err != nil {
+// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 			return
+// 		}
+
+// 		// Use default true if IsAvailable is not provided.
+// 		isAvailable := true
+// 		if req.IsAvailable != nil {
+// 			isAvailable = *req.IsAvailable
+// 		}
+
+// 		menuItem, err := store.CreateMenuItem(c, db.CreateMenuItemParams{
+// 			RestaurantID: req.RestaurantID,
+// 			Name:         req.Name,
+// 			Description: pgtype.Text{
+// 				String: req.Description,
+// 				Valid:  req.Description != "",
+// 			},
+// 			Price: pgtype.Numeric{
+// 				String: req.Price,
+// 				Valid:  true,
+// 			},
+// 			IsAvailable: pgtype.Bool{
+// 				Bool:  isAvailable,
+// 				Valid: true,
+// 			},
+// 		})
+// 		if err != nil {
+// 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create menu item"})
+// 			return
+// 		}
+
+// 		c.JSON(http.StatusOK, menuItem)
+// 	}
+// }
